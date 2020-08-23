0d4e73211a4467a4f92a361786377ecdf814e81c
tangzhenyu <321315491@qq.com>
2020-08-24 02:02:10 +0800

    项目上传

--- YanAn/Assets/Scripts/Logic/ImpactChecking.cs
+++ YanAn/Assets/Scripts/Logic/ImpactChecking.cs
@@ -4,21 +4,42 @@ using UnityEngine;
 
 public class ImpactChecking : MonoBehaviour
 {
+    private TipsPanel tipsPanel;
     private void OnCollisionEnter(Collision collision)
     {
         switch (collision.gameObject.name)
         {
             case "xintianyou":
-
+                 tipsPanel = GameManager.GetInstance.UIPanelManager.OpenPanel(GameAssetCache.tipsPanelPath) as TipsPanel;
+                tipsPanel.Initial("提示", "功能暂未开发，敬请期待!");
+                tipsPanel.InitButton(() =>
+                {
+                    GameManager.GetInstance.UIPanelManager.CloseBasePanel();
+                }, "确定");
                 break;
             case "baimaonv":
-
+                 tipsPanel = GameManager.GetInstance.UIPanelManager.OpenPanel(GameAssetCache.tipsPanelPath) as TipsPanel;
+                tipsPanel.Initial("提示", "功能暂未开发，敬请期待!");
+                tipsPanel.InitButton(() =>
+                {
+                    GameManager.GetInstance.UIPanelManager.CloseBasePanel();
+                }, "确定");
                 break;
             case "yanggequ":
-
+                 tipsPanel = GameManager.GetInstance.UIPanelManager.OpenPanel(GameAssetCache.tipsPanelPath) as TipsPanel;
+                tipsPanel.Initial("提示", "功能暂未开发，敬请期待!");
+                tipsPanel.InitButton(() =>
+                {
+                    GameManager.GetInstance.UIPanelManager.CloseBasePanel();
+                }, "确定");
                 break;
             case "taicipaiyan":
-
+                 tipsPanel = GameManager.GetInstance.UIPanelManager.OpenPanel(GameAssetCache.tipsPanelPath) as TipsPanel;
+                tipsPanel.Initial("提示", "功能暂未开发，敬请期待!");
+                tipsPanel.InitButton(() =>
+                {
+                    GameManager.GetInstance.UIPanelManager.CloseBasePanel();
+                }, "确定");
                 break;
             default:
                 break;
